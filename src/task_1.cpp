597df804e83f9749ebed7c41c51bb8fd29c943f3
Itcoderbek <128024527+Itcoderbek@users.noreply.github.com>
2023-11-07 16:52:16 +0500

    Update task_1.cpp
    cm

--- src/task_1.cpp
+++ src/task_1.cpp
@@ -1,1 +1,40 @@
+#include "task_1.h"
+#include <stdexcept>
+
+Stack::Stack() : head(nullptr) {}
+
+Stack::~Stack() {
+    while (!isEmpty()) {
+        pop();
+    }
+}
+
+void Stack::push(int data) {
+    Node* newNode = new Node;
+    newNode->data = data;
+    newNode->next = head;
+    head = newNode;
+}
+
+int Stack::pop() {
+    if (isEmpty()) {
+        throw std::out_of_range("Stack is empty");
+    }
+    int data = head->data;
+    Node* temp = head;
+    head = head->next;
+    delete temp;
+    return data;
+}
+
+int Stack::top() {
+    if (isEmpty()) {
+        throw std::out_of_range("Stack is empty");
+    }
+    return head->data;
+}
+
+bool Stack::isEmpty() {
+    return head == nullptr;
+}
 
